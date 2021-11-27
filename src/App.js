5ba568fd6ef7e3e6cde76115b41322d2df3e62dc
tianrenjiang <563944616@qq.com>
2021-11-27 09:46:13 +0800

    git push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
+import { Routes,Route} from 'react-router-dom'
+import Admin from './pages/Admin'
+import Login from './pages/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+
+  render() {
+    return (
+        <Routes>
+          <Route path="/admin" element={<Admin />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+    )
+  }
 }
-
-export default App;
